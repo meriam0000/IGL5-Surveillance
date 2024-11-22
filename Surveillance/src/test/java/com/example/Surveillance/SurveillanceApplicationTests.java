bd5ecf83a16456d62db4ebe1d3dd9e4b5761f09a
LENOVO <merriambelhadjhamida@gmail.com>
2024-11-22 22:19:48 +0100

    first commit

---
+++ Surveillance/src/test/java/com/example/Surveillance/SurveillanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Surveillance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveillanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
