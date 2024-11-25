5771344052300e5bf6b0008a94218aca6c76b684
aymen1508 <aymen.skiken@gmail.com>
2024-11-25 22:37:37 +0100

    Refactor SalleController to use DTOs and Services

--- Surveillance/src/main/java/com/example/Surveillance/controllers/SalleController.java
+++ Surveillance/src/main/java/com/example/Surveillance/controllers/SalleController.java
@@ -1,60 +1,56 @@
 package com.example.Surveillance.Controllers;
 
-import com.example.Surveillance.Entities.Salle;
-import com.example.Surveillance.ServiceImp.SalleServiceImp;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.Surveillance.Dtos.SalleDto;
+import com.example.Surveillance.Services.SalleService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
-
-
-
-@RestController
-@RequestMapping("/salles")
+@Controller
+@RestController("/api/v1/salles")
+@AllArgsConstructor
 public class SalleController {
-
-    @Autowired
-    private SalleServiceImp salleService;
+    final private SalleService salleService;
 
     @GetMapping
-    public List<Salle> getAllSalles() {
-        return salleService.getSalles();
+    public ResponseEntity<List<SalleDto>> findAll() {
+        List<SalleDto> salles = salleService.getAllSalles();
+        return ResponseEntity.ok(salles);
+    }
+    @PostMapping
+    public ResponseEntity<SalleDto> addSalle(@RequestBody SalleDto salleDto) {
+        return ResponseEntity.ok(salleService.addSalle(salleDto));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Salle> getSalleById(@PathVariable Long id) {
-        Salle salle = salleService.getAllSalle(id).get();
-        if (salle != null) {
-            return ResponseEntity.ok(salle);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @PostMapping
-    public Salle createSalle(@RequestBody Salle salle) {
-        return salleService.createSalle(salle);
+    public ResponseEntity<SalleDto> findById(@PathVariable Long id) {
+        SalleDto salle = salleService.getSalleById(id);
+        return ResponseEntity.ok(salle);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Salle> updateSalle(@PathVariable Long id, @RequestBody Salle salleDetails) {
-        Salle updatedSalle = salleService.updateSalle(id, salleDetails);
-        if (updatedSalle != null) {
-            return ResponseEntity.ok(updatedSalle);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<SalleDto> UpdateSalle(
+            @PathVariable Long id,
+            @RequestBody SalleDto salleDto
+    ) {
+        SalleDto updatedSalle = salleService.updateSalle(id, salleDto);
+        return ResponseEntity.ok(updatedSalle);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remove(@PathVariable Long id) {
+        salleService.deleteSalle(id);
+        return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteSalle(@PathVariable Long id) {
-        boolean isDeleted = salleService.deleteSalle(id);
-        if (isDeleted) {
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+
+
+
+
+
+
+
+
 }
