5739c8ffb74a345f9d3d441869371f781ff324c0
Mohamed Aziz Idoudi <mohamedaziz.idoudi@etudiant-fst.utm.tn>
2025-01-16 20:05:41 +0100

    cors

--- Surveillance/src/main/java/com/example/Surveillance/SurveillanceApplication.java
+++ Surveillance/src/main/java/com/example/Surveillance/SurveillanceApplication.java
@@ -3,11 +3,10 @@ package com.example.Surveillance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.Surveillance")
 public class SurveillanceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SurveillanceApplication.class, args);
 	}
-
 }
+
