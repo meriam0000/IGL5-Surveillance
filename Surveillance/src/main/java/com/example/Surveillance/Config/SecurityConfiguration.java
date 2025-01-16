c1374a729a5d9bc611302d593b23ec332470c778
LENOVO <merriambelhadjhamida@gmail.com>
2025-01-16 20:08:31 +0100

    cors correction

--- Surveillance/src/main/java/com/example/Surveillance/Config/SecurityConfiguration.java
+++ Surveillance/src/main/java/com/example/Surveillance/Config/SecurityConfiguration.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
